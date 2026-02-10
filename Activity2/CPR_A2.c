78d6d0465dda39850de2220bb2307ac2aeecfd55
Dil1797 <147470344+Dil1797@users.noreply.github.com>
2026-02-10 18:05:54 -0500

    Create CPR_A2.c

---
+++ Activity2/CPR_A2.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	printf("My name is Dil Humyra Sultana Borna.\n");
+	printf("My teacher's name is Andrei Sajeniouk.\n");
+	printf("My section ID is CPR101NFF.04629.2241.\n");
+	printf("The activity submission date is 21st January, 2024.\n");
+
+	return 0;
+}
